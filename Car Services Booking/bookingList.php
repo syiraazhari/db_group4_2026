f6078d3152036643ce81b5207575c3c9747be506
alya karmilla <karmillahopeee@gmail.com>
2026-06-11 03:26:52 +0800

    update booking list
    update nama database

--- Car Services Booking/bookingList.php
+++ Car Services Booking/bookingList.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$conn = mysqli_connect("localhost", "root", "", "carservicesbooking");
+$conn = mysqli_connect("localhost", "root", "", "carservicebooking(1)");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_error($conn));
