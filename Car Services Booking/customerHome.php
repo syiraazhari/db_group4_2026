b8801b7c5298fbcb29c6971a6951c97c85a176b2
alya karmilla <karmillahopeee@gmail.com>
2026-06-11 00:54:18 +0800

    updated homepage
    removed //add

--- Car Services Booking/customerHome.php
+++ Car Services Booking/customerHome.php
@@ -6,11 +6,11 @@
 	</head>
 <body>
 	<div class="content">
-		<button a href=" viewBookings.php"> My Bookings Appointment </a> </button> //dah
-		<button a href="customerVehicles.php"> My Vehicles </a> </button> //dah
-		<button a href="registerBooking.php"> Book An Appointment </a> </button> //dah
-		<button a href="profilecustomer.php"> My Profile </a> </button> // dah
-		<button a href="logoutprocess.php"> Log Out </a> </button> // dah
+		<button a href=" viewBookings.php"> My Bookings Appointment </a> </button>
+		<button a href="customerVehicles.php"> My Vehicles </a> </button>
+		<button a href="registerBooking.php"> Book An Appointment </a> </button>
+		<button a href="profilecustomer.php"> My Profile </a> </button>
+		<button a href="logoutprocess.php"> Log Out </a> </button>
 	</div>
 </body>
 </html>
