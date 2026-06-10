564738d5275f2795b5d7506e3cc61d7699ac8b4f
alya karmilla <karmillahopeee@gmail.com>
2026-06-10 11:32:18 +0800

    customer registration correction
    added framework and

--- registerCustomer.php
+++ registerCustomer.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New Registration</title>
     <link href="booking.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
-<body>
+<body class="p-4">
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
@@ -20,7 +21,7 @@
                                 <label for="customer_fname" class="form-label">First Name:</label>
                                 <input type="text" class="form-control" id="customer_fname" name="fname" required>
                             </div>
-							</div>
+						
                             <div class="mb-3">
                                 <label for="customer_lname" class="form-label">Last Name:</label>
                                 <input type="text" class="form-control" id="customer_lname" name="lname" required>
@@ -35,7 +36,7 @@
                             </div>
 							<div class="mb-3">
                                 <label for="bday" class="form-label">Birthday:</label>
-                                <input type="text" class="form-control" id="bday" name="birthday" required>
+                                <input type="date" class="form-control" id="bday" name="birthday" required>
                             </div>
 							<div class="mb-3">
                                 <label for="email" class="form-label">Email:</label>
@@ -50,7 +51,6 @@
                                 <input type="password" class="form-control" id="password" name="password"  placeholder="Abc@123" required>
                             </div>
                             <button type="submit" name="add" class="btn btn-primary">Register</button>
-                            <a href="customerList.php" class="btn btn-secondary">Cancel</a>
                         </form>
                     </div>
                 </div>
